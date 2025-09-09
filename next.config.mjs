b5dbfd89c737ededcd256b5d9932ce163e77ae62
luisfboff1 <luisfboff@gmail.com>
2025-09-09 14:13:00 -0300

    Fix path mapping in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,13 @@
 const nextConfig = {
   experimental: {
     serverActions: { allowedOrigins: ['*'] }
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname, 'src'),
+    };
+    return config;
   }
 };
 export default nextConfig;
